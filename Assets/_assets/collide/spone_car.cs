6b154f9331fcdedf403dda772c9c6d77d55716df
phamviet11211 <phamviet4454@gmail.com>
2025-07-28 19:09:57 +0700

    edit_logic

--- Assets/_assets/collide/spone_car.cs
+++ Assets/_assets/collide/spone_car.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class spone_car : MonoBehaviour
@@ -25,10 +26,13 @@ public class spone_car : MonoBehaviour
     public void SpawnCars(int quantity_cars)
     {
         GameObject[] randomCars = GetRandomCars(list_cars, quantity_cars);
-       for(int i=0; i<randomCars.Length; i++)
+        GameObject[] randomLocations = GetRandomLocation(location, randomCars.Length);
+
+       for (int i=0; i<randomCars.Length; i++)
         {
-            int randomIndex_location = Random.Range(0, location.Length);
-            Instantiate(randomCars[i], location[randomIndex_location].transform.position, randomCars[i].transform.rotation);
+            // int randomIndex_location = Random.Range(0, location.Length);
+            // Instantiate(randomCars[i], location[randomIndex_location].transform.position, randomCars[i].transform.rotation);
+            Instantiate(randomCars[i], randomLocations[i].transform.position, randomCars[i].transform.rotation);
         }    
     }
 
@@ -43,23 +47,52 @@ public class spone_car : MonoBehaviour
         return randomCars;
     }
 
+    GameObject[] GetRandomLocation(GameObject[] location, int indexCars)
+    {
+
+        // Copy mảng gốc để tránh làm thay đổi mảng ban đầu
+        List<GameObject> shuffled = new List<GameObject>(location);
+
+        // Fisher-Yates Shuffle
+        for (int i = shuffled.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            GameObject temp = shuffled[i];
+            shuffled[i] = shuffled[j];
+            shuffled[j] = temp;
+        }
+
+        // Lấy số lượng bằng số lượng indexCars
+        int count = Mathf.Min(indexCars, shuffled.Count);
+        GameObject[] randomLocation = shuffled.GetRange(0, count).ToArray();
+        return randomLocation;
+    }
+
+
+
     public void SpawnCars_1_1(int quantity_cars)
     {
         int randomSpamQuantityCar = Random.Range(0, 3);
         int randomSpamQuantityCar_1_1 = Random.Range(0, 3);
+        GameObject[] randomLocations = GetRandomLocation(location, randomSpamQuantityCar);
+        GameObject[] randomLocations_1_1 = GetRandomLocation(location_1_1, randomSpamQuantityCar_1_1);
 
 
         GameObject[] randomCars = GetRandomCars(list_cars, randomSpamQuantityCar);
         for (int i = 0; i < randomCars.Length; i++)
         {
-            int randomIndex_location = Random.Range(0, location.Length);
-            Instantiate(randomCars[i], location[randomIndex_location].transform.position, randomCars[i].transform.rotation);
+            //int randomIndex_location = Random.Range(0, location.Length);
+            //Instantiate(randomCars[i], location[randomIndex_location].transform.position, randomCars[i].transform.rotation);
+            Instantiate(randomCars[i], randomLocations[i].transform.position, randomCars[i].transform.rotation);
+
         }
         GameObject[] randomCars_1_1 = GetRandomCars(list_cars_1_1, randomSpamQuantityCar_1_1);
         for (int i = 0; i < randomCars_1_1.Length; i++)
         {
-            int randomIndex_location = Random.Range(0, location_1_1.Length);
-            Instantiate(randomCars_1_1[i], location_1_1[randomIndex_location].transform.position, randomCars_1_1[i].transform.rotation);
+            //int randomIndex_location = Random.Range(0, location_1_1.Length);
+            // Instantiate(randomCars_1_1[i], location_1_1[randomIndex_location].transform.position, randomCars_1_1[i].transform.rotation);
+            Instantiate(randomCars_1_1[i], randomLocations_1_1[i].transform.position, randomCars_1_1[i].transform.rotation);
+
         }
     }
 
